b78ae1e8a96120e746bbbe9df75f5b0f36c8fedf
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2021-10-21 20:27:08 +0600

    Done

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>I am inside inventory</h1>
+            <h1>Now I am inside inventory</h1>
         </div>
     );
 };
